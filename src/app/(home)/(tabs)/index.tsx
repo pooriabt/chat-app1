8c4e13e03264eafe9faab9d13a1c7eda6298c333
pooriabt <pooria.tavakoly@gmail.com>
2025-04-18 14:01:26 +0330

    List of users

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,14 +1,30 @@
-import { router } from "expo-router";
-
+import { Link, router, Stack } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
 import { ChannelList } from "stream-chat-expo";
 import { useAuth } from "../../../providers/AuthProvider";
 
 export default function MainTabScreen() {
   const { user } = useAuth();
   return (
-    <ChannelList
-      filters={{ members: { $in: [user.id] } }}
-      onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
-    />
+    <>
+      <Stack.Screen
+        options={{
+          headerRight: () => (
+            <Link href={"/(home)/users"} asChild>
+              <Entypo
+                name="users"
+                size={22}
+                color="gray"
+                style={{ marginHorizontal: 15 }}
+              />
+            </Link>
+          ),
+        }}
+      />
+      <ChannelList
+        filters={{ members: { $in: [user.id] } }}
+        onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
+      />
+    </>
   );
 }
